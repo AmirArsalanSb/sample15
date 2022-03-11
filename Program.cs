fcb9e32a919adcf6664e5d92aa25a400acc846be
amir <amir6665aqq@gmail.com>
2022-03-11 14:24:23 +0330

    first commit

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿int number; 
+System.Console.WriteLine("mah ro bede");
+number=Convert.ToInt32(Console.ReadLine());
+switch (number)
+{
+case 1 :
+System.Console.WriteLine("january");
+break;
+case 2 :
+System.Console.WriteLine("february");
+break;
+case 3 :
+System.Console.WriteLine("march");
+break;
+case 4 :
+System.Console.WriteLine("April");
+break;
+case 5 :
+System.Console.WriteLine("May");
+break;
+case 6 :
+System.Console.WriteLine("june");
+break;
+case 7 :
+System.Console.WriteLine("july");
+break;
+case 8 :
+System.Console.WriteLine("August");
+break;
+case 9 :
+System.Console.WriteLine("september");
+break;
+case 10 :
+System.Console.WriteLine("october");
+break;
+case 11 :
+System.Console.WriteLine("November");
+break;
+case 12 :
+System.Console.WriteLine("December");
+break;
+default : 
+System.Console.WriteLine("nadarim");
+break;
+}
+
